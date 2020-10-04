c2f83a9a79e652d3b5ee60bee3cb98c592705b80
James Adriano <james.patrick.adriano@gmail.com>
2020-10-04 08:28:13 +0800

    structure routes

--- routers/router.go
+++ routers/router.go
@@ -8,24 +8,28 @@ import (
 
 func init() {
 	test := &controllers.MainController{}
-
-	beego.Router("/", test)
-
-	beego.Router(`/debug/pprof`, &controllers.ProfController{})
-	beego.Router(`/debug/pprof/:pp([\w]+)`, &controllers.ProfController{})
-
 	user := &controllers.UserController{}
 	todo := &controllers.TodoController{}
 
-	beego.Router("/user/:id", user, "get:Get")
-	beego.Router("/user/signup", user, "post:Signup")
-	beego.Router("/user/login", user, "post:Login")
-	beego.Router("/user/update/:id", user, "put:Update")
-	beego.Router("/user/delete/:id", user, "delete:Delete")
+	beego.Router("/", test)
 
-	beego.Router("/todo/:id", todo, "get:Get")
-	beego.Router("/todo/create", todo, "post:Create")
-	beego.Router("/todo/update/:id", todo, "put:Update")
-	beego.Router("/todo/delete/:id", todo, "delete:Delete")
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/user",
+			beego.NSRouter("/:id", user, "get:Get;put:Update;delete:Delete"),
+			beego.NSRouter("/signup", user, "post:Signup"),
+			beego.NSRouter("/login", user, "post:Login"),
+		),
+		beego.NSNamespace("/todo",
+			beego.NSRouter("/:id", todo, "get:Get;put:Update;delete:Delete"),
+			beego.NSRouter("/", todo, "post:Create"),
+		),
+		beego.NSNamespace("/pprof",
+			beego.NSRouter(`/:pp([\w]+)`, &controllers.ProfController{}),
+			beego.NSRouter(`/`, &controllers.ProfController{}),
+		),
+	)
+
+	beego.AddNamespace(ns)
 
 }
